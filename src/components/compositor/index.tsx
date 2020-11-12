cce472c77ca209d2b9db1411a9ff7c33143d3149
Scott Gibbs <scott@gibbz.biz>
2020-11-12 11:58:18 -0600

    Disabled the ability to select the initial info text

--- src/components/compositor/index.tsx
+++ src/components/compositor/index.tsx
@@ -59,12 +59,12 @@ export default function Compositor() {
 /** Renders the initial info section within the compositor before the user has selected an image. */
 function InfoSection({ isDropping, isError }: { isDropping: boolean, isError: boolean }) {
     return <Fragment>
-        <h2 class="max-w-sm mx-auto space-x-1 text-xl sm:text-3xl text-center font-open font-semibold">
+        <h2 class="max-w-sm mx-auto space-x-1 text-xl sm:text-3xl text-center font-open font-semibold select-none">
             Make snapshots look
             <svg class="inline w-6 h-6 sm:w-10 sm:h-10 mx-1 text-orange-500" fill="currentColor" viewBox="0 1 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z" clip-rule="evenodd"></path></svg>
             with a pretty background
         </h2>
-        <div class={join("w-full py-4 px-6 space-y-2 text-base text-center sm:text-lg rounded-3xl border-4",
+        <div class={join("w-full py-4 px-6 space-y-2 text-base text-center sm:text-lg rounded-3xl border-4 select-none",
             isDropping ? "border-solid" : "border-dashed",
             isError ? "border-red-500" : "border-gray-400")}>
             <p class="font-semibold text-lg sm:text-2xl">Get started</p>
