15e449669b0266e4fd07119328394ce39356b931
Scott Gibbs <scott@gibbz.biz>
2020-11-17 23:42:49 -0600

    Added messages for unsupported browsers

--- global.d.ts
+++ global.d.ts
@@ -11,3 +11,11 @@ declare module "*.jpg" { const value: any; export = value; }
 declare module "*.svg" { const value: any; export = value; }
 
 declare function plausible(event: string): void
+
+declare var InstallTrigger: {} | undefined
+declare interface Window {
+    chrome: {
+        webstore: {} | undefined
+        runtime: {} | undefined
+    }
+}
