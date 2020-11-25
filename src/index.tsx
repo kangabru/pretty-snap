d27aabc6dc8bb1ca4ec80a66705f32c52082f675
Scott Gibbs <scott@gibbz.biz>
2020-11-25 13:48:36 -0600

    Made the product hunt link inline

--- src/index.tsx
+++ src/index.tsx
@@ -67,7 +67,7 @@ function Info() {
 }
 
 function PH() {
-    return <a class=" md:fixed bottom-5 right-5" target="_blank"
+    return <a target="_blank"
         href="https://www.producthunt.com/posts/pretty-snap?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-pretty-snap">
         <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=275698&theme=light" alt="Pretty Snap - Make screenshots look 🔥 with a pretty background | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" />
     </a>
