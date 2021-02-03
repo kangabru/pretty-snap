995205f546fc18347d509a2fc063a4e5e79d218c
Scott Gibbs <scott@gibbz.biz>
2021-02-02 23:45:52 -0600

    Abstracted the common box and line containers into components

--- src/pretty-annotate/components/annotations/box.tsx
+++ src/pretty-annotate/components/annotations/box.tsx
@@ -3,6 +3,7 @@ import { animated } from 'react-spring';
 import useNiceDashLength from '../../hooks/use-dash';
 import { DASH, STROKE } from '../../misc/constants';
 import { Style, StyleData } from '../../misc/types';
+import { Bounds } from '../compositor/resizer';
 
 type BoxProps = StyleData<Style.Box>
 
@@ -10,36 +11,39 @@ export default function Box(props: BoxProps) {
     return props.dashed ? <BoxDashed {...props} /> : <BoxSolid {...props} />
 }
 
-function BoxSolid({ left, top, width, height }: BoxProps) {
-    const strokeWidth = STROKE, strokeMargin = strokeWidth / 2
-
-    // Adjust the bounds by half the stroke width so the svg doesn't clip off the edges
-    const x1 = strokeMargin, y1 = strokeMargin
-
+function BoxContainer({ children, left, top, width, height }: Bounds & JSX.ElementChildrenAttribute) {
+    const strokeWidth = STROKE, strokeMargin = strokeWidth / 2 // Adjust the bounds so svg doesn't clip stroke edges
     return <div class="absolute" style={{ left: left - strokeMargin, top: top - strokeMargin }}>
         <svg fill="currentColor" width={width + strokeWidth} height={height + strokeWidth} xmlns="http://www.w3.org/2000/svg">
-            <rect x={x1} y={y1} width={width} height={height} fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth={strokeWidth} />
+            {children}
         </svg>
     </div>
 }
 
-function BoxDashed({ left, top, width, height }: BoxProps) {
+function BoxSolid(props: BoxProps) {
+    const { width, height } = props
+    const strokeMargin = STROKE / 2, x1 = strokeMargin, y1 = strokeMargin
+    return <BoxContainer {...props}>
+        <rect x={x1} y={y1} width={width} height={height} fill="none" stroke="currentColor" strokeLinejoin="round" strokeWidth={STROKE} />
+    </BoxContainer>
+}
+
+function BoxDashed(props: BoxProps) {
+    const { width, height } = props
     const strokeWidth = STROKE, strokeMargin = strokeWidth / 2
 
-    const dashProps = { stroke: "currentColor", strokeLinecap: "round" as any, strokeWidth }
     const [dashArrayW, dashOffsetW] = useNiceDashLength(width, DASH, true)
     const [dashArrayH, dashOffsetH] = useNiceDashLength(height, DASH, true)
+    const dashProps = { stroke: "currentColor", strokeLinecap: "round" as any, strokeWidth }
 
     // Adjust the bounds by half the stroke width so the svg doesn't clip off the edges
     const x1 = strokeMargin, y1 = strokeMargin
     const x2 = x1 + width, y2 = y1 + height
 
-    return <div class="absolute" style={{ left: left - strokeMargin, top: top - strokeMargin }}>
-        <svg fill="currentColor" width={width + strokeWidth} height={height + strokeWidth} xmlns="http://www.w3.org/2000/svg">
-            <animated.line x1={x1} y1={y1} x2={x2} y2={y1} {...dashProps} strokeDasharray={dashArrayW} strokeDashoffset={dashOffsetW} /> {/* Top */}
-            <animated.line x1={x1} y1={y2} x2={x2} y2={y2} {...dashProps} strokeDasharray={dashArrayW} strokeDashoffset={dashOffsetW} /> {/* Bottom */}
-            <animated.line x1={x1} y1={y1} x2={x1} y2={y2} {...dashProps} strokeDasharray={dashArrayH} strokeDashoffset={dashOffsetH} /> {/* Left */}
-            <animated.line x1={x2} y1={y1} x2={x2} y2={y2} {...dashProps} strokeDasharray={dashArrayH} strokeDashoffset={dashOffsetH} /> {/* Right */}
-        </svg>
-    </div>
+    return <BoxContainer {...props}>
+        <animated.line x1={x1} y1={y1} x2={x2} y2={y1} {...dashProps} strokeDasharray={dashArrayW} strokeDashoffset={dashOffsetW} /> {/* Top */}
+        <animated.line x1={x1} y1={y2} x2={x2} y2={y2} {...dashProps} strokeDasharray={dashArrayW} strokeDashoffset={dashOffsetW} /> {/* Bottom */}
+        <animated.line x1={x1} y1={y1} x2={x1} y2={y2} {...dashProps} strokeDasharray={dashArrayH} strokeDashoffset={dashOffsetH} /> {/* Left */}
+        <animated.line x1={x2} y1={y1} x2={x2} y2={y2} {...dashProps} strokeDasharray={dashArrayH} strokeDashoffset={dashOffsetH} /> {/* Right */}
+    </BoxContainer>
 }
--- src/pretty-annotate/components/annotations/line.tsx
+++ src/pretty-annotate/components/annotations/line.tsx
@@ -3,6 +3,7 @@ import { animated } from 'react-spring';
 import useNiceDashLength from '../../hooks/use-dash';
 import { DASH, STROKE } from '../../misc/constants';
 import { Style, StyleData } from '../../misc/types';
+import { Bounds } from '../compositor/resizer';
 import { GetArrowHeadMargin } from './arrow';
 
 type LineProps = StyleData<Style.Line>
@@ -11,42 +12,43 @@ export default function Line(props: LineProps) {
     return props.dashed ? <LineDashed {...props} /> : <LineSolid {...props} />
 }
 
-export function LineSolid({ children, ...props }: LineProps & { children?: JSX.Element }) {
+function LineContainer({ children, left, top, width, height }: Bounds & JSX.ElementChildrenAttribute) {
     const margin = GetArrowHeadMargin(), strokeMargin = STROKE / 2
-
-    const { left, top, width, height } = props
-    const [x1, y1, x2, y2] = GetLineCoords(props, margin, margin)
-
     return <div class="absolute" style={{ left: left - margin - strokeMargin, top: top - margin - strokeMargin }}>
         <svg fill="currentColor" width={width + 2 * margin} height={height + 2 * margin} xmlns="http://www.w3.org/2000/svg">
-            <line x1={x1} y1={y1} x2={x2} y2={y2} fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth={STROKE} />
             {children}
         </svg>
     </div>
 }
 
-export function LineDashed({ children, ...props }: LineProps & { children?: JSX.Element }) {
-    const margin = GetArrowHeadMargin(), strokeMargin = STROKE / 2
-    const dashProps = { stroke: "currentColor", strokeLinecap: "round" as any, strokeWidth: STROKE }
+export function LineSolid({ children, ...props }: LineProps & { children?: JSX.Element }) {
+    const margin = GetArrowHeadMargin()
+    const [x1, y1, x2, y2] = GetLineCoords(props, margin, margin)
 
-    const { left, top, width, height } = props
+    return <LineContainer {...props}>
+        <line x1={x1} y1={y1} x2={x2} y2={y2} fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth={STROKE} />
+        {children}
+    </LineContainer>
+}
+
+export function LineDashed({ children, ...props }: LineProps & { children?: JSX.Element }) {
+
+    const { width, height } = props
+    const margin = GetArrowHeadMargin()
     const [x1, y1, x2, y2] = GetLineCoords(props, margin, margin)
 
     const lineLength = Math.sqrt(width ** 2 + height ** 2)
     const [dashArray, dashOffset] = useNiceDashLength(lineLength, DASH)
+    const dashProps = { stroke: "currentColor", strokeLinecap: "round" as any, strokeWidth: STROKE }
 
-    return <div class="absolute" style={{ left: left - margin - strokeMargin, top: top - margin - strokeMargin }}>
-        <svg fill="currentColor" width={width + 2 * margin} height={height + 2 * margin} xmlns="http://www.w3.org/2000/svg">
-            <animated.line x1={x1} y1={y1} x2={x2} y2={y2} {...dashProps} strokeDasharray={dashArray} strokeDashoffset={dashOffset} />
-            {children}
-        </svg>
-    </div>
+    return <LineContainer {...props}>
+        <animated.line x1={x1} y1={y1} x2={x2} y2={y2} {...dashProps} strokeDasharray={dashArray} strokeDashoffset={dashOffset} />
+        {children}
+    </LineContainer>
 }
 
 export function GetLineCoords({ width, height, negX, negY }: LineProps, dx = 0, dy = 0) {
     const strokeWidth = STROKE, strokeMargin = strokeWidth / 2
-
-    // Adjust the bounds by half the stroke width so the svg doesn't clip off the edges
     const x1 = strokeMargin + (negX ? width : 0)
     const y1 = strokeMargin + (negY ? height : 0)
     const x2 = x1 + (negX ? -1 : 1) * width
