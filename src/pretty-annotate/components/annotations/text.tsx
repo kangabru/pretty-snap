1c236fdbfcf05d64b41c039aa6f675e86cf50265
Scott Gibbs <scott@gibbz.biz>
2021-02-08 17:06:21 -0600

    Improved text editing and code

--- src/pretty-annotate/components/annotations/text.tsx
+++ src/pretty-annotate/components/annotations/text.tsx
@@ -1,26 +1,48 @@
 import { h } from 'preact';
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
 import { useDocumentListener } from '../../../common/hooks/misc';
-import { join, onKeys } from '../../../common/misc/utils';
+import { join, onKeys, useRandomItem } from '../../../common/misc/utils';
 import { Annotation, Style } from '../../misc/types';
 import useAnnotateStore from '../../stores/annotation';
 
-const DEFAULT_TEXT = "Hello"
+type Props = Annotation<Style.Text>
 
-export default function Text(props: Annotation<Style.Text>) {
-    const { id, left, top, colour, text } = props
+const DEFAULT_TEXTS = [
+    "Hello",
+    "Oh hey",
+    "Hi 👋",
+]
+
+const CLS = "hover:cursor-crosshair absolute px-2 py-1 rounded-lg text-white font-bold text-xl font-mono grid place-items-center select-none transform -translate-x-6 -translate-y-6 ring-gray-200 ring-opacity-60"
+
+export default function Text(props: Props) {
+    const { id, text } = props
+
+    const canEdit = useAnnotateStore(s => s.style.type == Style.Text)
+    const editable = useAnnotateStore(s => canEdit && id && s.idEditing === id)
+
+    const edit = useAnnotateStore(s => s.edit)
+    const editOnClick = (e: Event) => { if (id) { edit(id); e.stopPropagation() } }
+
+    const emptyText = useRandomItem(DEFAULT_TEXTS)
+
+    return editable
+        ? <TextInput {...props} />
+        : <span style={getStyle(props)} class={join(CLS, canEdit ? "cursor-text pointer-events-auto" : "pointer-events-none")}
+            onClick={canEdit ? editOnClick : undefined}>
+            {text ?? emptyText}
+        </span>
+}
+
+function TextInput(props: Props) {
+    const { id, text } = props
 
     const ref = useRef<HTMLInputElement>()
+    const [textEdits, setTextEdits] = useState(text)
 
-    const editable = useAnnotateStore(s => id && s.idEditing === id)
-    const edit = useAnnotateStore(s => s.edit)
     const stop = useAnnotateStore(s => s.editStop)
     const saveText = useAnnotateStore(s => s.saveAnnotation)
-
-    const [textEdits, setTextEdits] = useState("")
-    useEffect(() => {
-        if (editable) setTextEdits(text ?? DEFAULT_TEXT)
-    }, [text, editable])
+    const editable = useAnnotateStore(s => id && s.idEditing === id)
 
     const save = () => {
         if (!editable) return
@@ -28,25 +50,17 @@ export default function Text(props: Annotation<Style.Text>) {
         else saveText({ ...props, text: textEdits }, !text) // Creates an undo event
     }
 
-    const stopPropagation = (e: Event) => e.stopPropagation()
-    const editOnClick = (e: Event) => {
-        if (!id) return
-        edit(id)
-        ref.current?.focus()
-        e.stopPropagation()
-    }
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
     useLayoutEffect(() => void ref.current?.focus(), [ref.current])
     useDocumentListener('mousedown', save, [editable])
 
-    const style = { left, top, backgroundColor: colour }
-    const cls = join(editable && "ring-4", "hover:cursor-crosshair absolute px-2 py-1 rounded-lg text-white font-bold text-xl font-mono grid place-items-center select-none transform -translate-x-6 -translate-y-6 ring-gray-200 ring-opacity-60")
+    return <input ref={ref as any} value={textEdits}
+        style={getStyle(props)} class={join(CLS, "ring-4 pointer-events-auto")}
+        onMouseDown={e => e.stopPropagation()}
+        onKeyDown={onKeys({ 'Escape': save, 'Enter': save })}
+        onInput={e => setTextEdits(e.currentTarget.value)} />
+}
 
-    return editable
-        ? <input ref={ref as any} class={cls} style={style} value={textEdits}
-            onMouseDown={stopPropagation}
-            onKeyDown={onKeys({ 'Escape': save, 'Enter': save })}
-            onInput={e => setTextEdits(e.currentTarget.value)} />
-        : <span class={join(cls, "cursor-text")} style={style} onClick={editOnClick}>{text ?? DEFAULT_TEXT}</span>
+function getStyle({ top, left, colour }: Props) {
+    return { left, top, backgroundColor: colour }
 }
