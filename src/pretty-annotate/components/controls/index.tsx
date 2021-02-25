6e8492443bd9e907e1e3d603fee731f262b3fe37
Scott Gibbs <scott@gibbz.biz>
2021-02-24 20:13:57 -0600

    Updated the position of control buttons into fit into 2 rows

--- src/pretty-annotate/components/controls/index.tsx
+++ src/pretty-annotate/components/controls/index.tsx
@@ -11,15 +11,18 @@ import useOptionsStore from '../../stores/options';
 import { GetBracketPaths } from '../annotations/bracket';
 
 export default function Controls(props: Exports) {
-    return <section class="hidden col max-w-xl w-full mx-auto">
-        <ShapeButtonGroup />
-        <ShapeStyleButtonGroup />
-        <ColorButtonGroup />
+    return <section class="col max-w-xl w-full mx-auto space-y-3">
+        <ButtonRowWithAnim>
+            <ShapeButtonGroup />
+        </ButtonRowWithAnim>
 
-        <div class="flex text-gray-800">
+        <ButtonRowWithAnim>
+            <ColorButtonGroup />
+            <ShapeStyleButtonGroup />
+
             <HistoryButtonGroup />
             <ExportButtonGroup {...props} />
-        </div>
+        </ButtonRowWithAnim>
 
         <ExportError {...props} />
     </section>
@@ -35,32 +38,30 @@ function ShapeButtonGroup() {
     const { style, setStyle } = useSetStyle()
     const { color: { color }, count } = style
 
-    return <ButtonRowWithAnim style={{ color }}>
-        <div class="flex justify-center space-x-3">
-            <StyleButton shape={Shape.Box}>
-                <rect x="2" y="2" width="16" height="16" rx="2" stroke="currentColor" fill='none' stroke-width="2.75" />
-            </StyleButton>
+    return <div class="flex justify-center space-x-3" style={{ color }}>
+        <StyleButton shape={Shape.Box}>
+            <rect x="2" y="2" width="16" height="16" rx="2" stroke="currentColor" fill='none' stroke-width="2.75" />
+        </StyleButton>
 
-            <StyleButton shape={Shape.Ellipse}>
-                <circle cx="10" cy="10" r="8" stroke="currentColor" fill='none' stroke-width="2.75" />
-            </StyleButton>
+        <StyleButton shape={Shape.Ellipse}>
+            <circle cx="10" cy="10" r="8" stroke="currentColor" fill='none' stroke-width="2.75" />
+        </StyleButton>
 
-            <StyleButton shape={Shape.Bracket}><BracketIcon /></StyleButton>
+        <StyleButton shape={Shape.Bracket}><BracketIcon /></StyleButton>
 
-            <StyleButton shape={Shape.Arrow}>
-                <line x1="4" y1="4" x2="16" y2="16" stroke="currentcolor" stroke-width="2.75" stroke-linecap="round" />
-                <line x1="6" y1="16" x2="16" y2="16" stroke="currentcolor" stroke-width="2.75" stroke-linecap="round" />
-                <line x1="16" y1="6" x2="16" y2="16" stroke="currentcolor" stroke-width="2.75" stroke-linecap="round" />
-            </StyleButton>
+        <StyleButton shape={Shape.Arrow}>
+            <line x1="4" y1="4" x2="16" y2="16" stroke="currentcolor" stroke-width="2.75" stroke-linecap="round" />
+            <line x1="6" y1="16" x2="16" y2="16" stroke="currentcolor" stroke-width="2.75" stroke-linecap="round" />
+            <line x1="16" y1="6" x2="16" y2="16" stroke="currentcolor" stroke-width="2.75" stroke-linecap="round" />
+        </StyleButton>
 
-            <StyleButton shape={Shape.Line}>
-                <line x1="4" y1="4" x2="16" y2="16" stroke="currentcolor" stroke-width="2.75" stroke-linecap="round" />
-            </StyleButton>
+        <StyleButton shape={Shape.Line}>
+            <line x1="4" y1="4" x2="16" y2="16" stroke="currentcolor" stroke-width="2.75" stroke-linecap="round" />
+        </StyleButton>
 
-            <AnnotateButtonSpan text={count ?? 1} onClick={setStyle({ shape: Shape.Counter })} />
-            <AnnotateButtonSpan text="T" onClick={setStyle({ shape: Shape.Text })} />
-        </div>
-    </ButtonRowWithAnim>
+        <AnnotateButtonSpan text={count ?? 1} onClick={setStyle({ shape: Shape.Counter })} />
+        <AnnotateButtonSpan text="T" onClick={setStyle({ shape: Shape.Text })} />
+    </div>
 }
 
 function ShapeStyleButtonGroup() {
@@ -72,67 +73,67 @@ function ShapeStyleButtonGroup() {
     const rowTransition = useRowTransition(canUseFill || canUseLine as boolean)
     const buttonTransitions = useRowButtonTransitions(items)
 
-    return rowTransition.map(({ item, props }) => item && <ButtonRow style={{ ...props, color: style.color.color }}>
-        <div class="flex justify-center">
-            {buttonTransitions.map(({ item, props, key }) => {
+    return rowTransition.map(({ item, props }) => item && <animated.div className="flex" style={{ ...props, color: style.color.color }}>
+        {buttonTransitions.map(({ item, props, key }) => {
 
-                if (item == 1) return <AnnotateButtonSvg key={key} style={props} onClick={setStyle({ style: {} })}>
-                    <line x1="4" y1="4" x2="16" y2="16" stroke="currentcolor" stroke-width="2.75" stroke-linecap="round" />
-                </AnnotateButtonSvg>
+            if (item == 1) return <AnnotateButtonSvg key={key} style={props} onClick={setStyle({ style: {} })}>
+                <line x1="4" y1="4" x2="16" y2="16" stroke="currentcolor" stroke-width="2.75" stroke-linecap="round" />
+            </AnnotateButtonSvg>
 
-                if (item == 2) return <AnnotateButtonSvg key={key} style={props} onClick={setStyle({ style: { dashed: true } })}>
-                    <line x1="4" y1="4" x2="16" y2="16" stroke="currentcolor" stroke-width="2.75" stroke-linecap="round" stroke-dasharray="2.5,5" stroke-dashoffset="0" />
-                </AnnotateButtonSvg>
+            if (item == 2) return <AnnotateButtonSvg key={key} style={props} onClick={setStyle({ style: { dashed: true } })}>
+                <line x1="4" y1="4" x2="16" y2="16" stroke="currentcolor" stroke-width="2.75" stroke-linecap="round" stroke-dasharray="2.5,5" stroke-dashoffset="0" />
+            </AnnotateButtonSvg>
 
-                if (item == 3) return <AnnotateButtonSvg key={key} style={props} onClick={setStyle({ style: { fillOpacity: 1 } })}>
-                    <rect x="2" y="2" width="16" height="16" rx="2" fill='currentColor' />
-                </AnnotateButtonSvg>
+            if (item == 3) return <AnnotateButtonSvg key={key} style={props} onClick={setStyle({ style: { fillOpacity: 1 } })}>
+                <rect x="2" y="2" width="16" height="16" rx="2" fill='currentColor' />
+            </AnnotateButtonSvg>
 
-                if (item == 4) return <AnnotateButtonSvg key={key} style={props} onClick={setStyle({ style: { fillOpacity: 0.3 } })}>
-                    <rect x="2" y="2" width="16" height="16" rx="2" fill='currentColor' opacity="0.5" />
-                </AnnotateButtonSvg>
-            })}
-        </div>
-    </ButtonRow>) as any
+            if (item == 4) return <AnnotateButtonSvg key={key} style={props} onClick={setStyle({ style: { fillOpacity: 0.3 } })}>
+                <rect x="2" y="2" width="16" height="16" rx="2" fill='currentColor' opacity="0.5" />
+            </AnnotateButtonSvg>
+        })}
+    </animated.div>) as any
 }
 
 function ColorButtonGroup() {
-    return <ButtonRowWithAnim>
-        <ColorButton color={colors.red} />
+    const color = colors.red
+    return <div class="flex">
+        <button style={{ backgroundColor: color }} class="w-12 h-12 rounded-md grid place-items-center" />
+        {/* <ColorButton color={colors.red} />
         <ColorButton color={colors.yellow} />
         <ColorButton color={colors.green} />
         <ColorButton color={colors.blue} />
         <ColorButton color={colors.dark} />
-        <ColorButton color={colors.light} useDarkText />
-    </ButtonRowWithAnim>
+        <ColorButton color={colors.light} useDarkText /> */}
+    </div>
 }
 
 function HistoryButtonGroup() {
     const undo = useAnnotateStore(s => s.undo)
     const redo = useAnnotateStore(s => s.redo)
-    return <ButtonRowWithAnim>
+    return <div class="flex">
         <AnnotateButtonSvg onClick={undo}>
             <path fill="currentColor" d="M8.445 14.832A1 1 0 0010 14v-2.798l5.445 3.63A1 1 0 0017 14V6a1 1 0 00-1.555-.832L10 8.798V6a1 1 0 00-1.555-.832l-6 4a1 1 0 000 1.664l6 4z"></path>
         </AnnotateButtonSvg>
         <AnnotateButtonSvg onClick={redo}>
             <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4.555 5.168A1 1 0 003 6v8a1 1 0 001.555.832L10 11.202V14a1 1 0 001.555.832l6-4a1 1 0 000-1.664l-6-4A1 1 0 0010 6v2.798l-5.445-3.63z"></path></svg>
         </AnnotateButtonSvg>
-    </ButtonRowWithAnim>
+    </div>
 }
 
 function ExportButtonGroup(props: Exports) {
     const image = useOptionsStore(s => s.image)
     const canExport = !!image?.src
-    return <ButtonRow>
+    return <div class="flex">
         <ExportButtons {...props} notReady={!canExport} />
-    </ButtonRow>
+    </div>
 }
 
 function useRowTransition(canShow: boolean) {
     return useTransition(canShow, null, {
-        from: { transform: 'scale(0)', padding: '0rem 0rem', margin: '0rem', opacity: 1 },
-        enter: { transform: 'scale(1)', padding: '0.75rem 0.375rem', margin: '0.5rem' },
-        leave: { transform: 'scale(0)', padding: '0rem 0rem', margin: '0rem', opacity: 0 },
+        from: { transform: 'scale(0)', padding: '0rem 0rem', margin: '0rem 0rem', opacity: 1 },
+        enter: { transform: 'scale(1)', padding: '0rem 0.375rem', margin: '0rem 0.5rem' },
+        leave: { transform: 'scale(0)', padding: '0rem 0rem', margin: '0rem 0rem', opacity: 0 },
     })
 }
 
@@ -151,7 +152,7 @@ function ButtonRowWithAnim({ children, style }: Children & CSSProps) {
 }
 
 function ButtonRow({ children, class: cls, ...props }: Children & CSSClass & AnimatedValue<ForwardedProps<any>>) {
-    return <animated.section {...props} className={join(cls, "flex p-3 space-x-3 bg-gray-200 max-w-lg rounded-lg m-2")}>{children}</animated.section>
+    return <animated.section {...props} className={join(cls, "col sm:flex-row justify-center space-y-5 sm:space-y-0 sm:space-x-8 p-3 rounded-lg bg-white shadow-md")}>{children}</animated.section>
 }
 
 function AnnotateButton({ children, ...props }: AnimatedValue<ForwardedProps<any>>) {
