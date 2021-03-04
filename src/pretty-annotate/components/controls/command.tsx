843a62b5438cd64f92fb119fcf10df02a3c741f9
Scott Gibbs <scott@gibbz.biz>
2021-03-03 20:42:32 -0600

    Hid keyboard command number on mobile

--- src/pretty-annotate/components/controls/command.tsx
+++ src/pretty-annotate/components/controls/command.tsx
@@ -5,7 +5,7 @@ import { join } from "../../../common/misc/utils";
 export type Command = { command?: string }
 
 export default function CommandText({ command, class: cls }: Command & CssClass) {
-    return command ? <span class={join(cls ?? "text-gray-400", "absolute bottom-px right-px text-sm leading-none transform scale-75")}>
+    return command ? <span class={join(cls ?? "text-gray-400", "hidden sm:block absolute bottom-px right-px text-sm leading-none transform scale-75")}>
         {command}
     </span> : null
 }
