904b3d4013069f915af45dd1415ac7f30b014c96
Scott Gibbs <scott@gibbz.biz>
2021-03-03 22:00:28 -0600

    Cleaned up the main page and added a quick screenshot guide

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import srcAnnotate from '../assets/images/preview-app-annotate.png';
 import srcBackground from '../assets/images/preview-app-background.jpg';
 import srcLogo from '../assets/images/thumb.jpg';
 import { routes, urls } from './common/constants';
+import { Children, CssClass } from './common/misc/types';
+import { join } from './common/misc/utils';
 import './index.css';
 import PrettyAnnotateApp from './pretty-annotate/index';
 import PrettyBackgroundApp from './pretty-background/index';
@@ -29,10 +31,10 @@ function PrettySnap() {
 function AppSelector() {
     return <>
         <section class="flex-1 col justify-center space-y-5">
-            <h2 class="text-2xl">
+            <h2 class="text-2xl text-center">
                 <span>Make your screenshots look</span>
                 <svg class="inline w-10 h-10 mx-1 -mt-3 text-orange-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z" clip-rule="evenodd"></path></svg>
-                <span>with these simple editing apps</span>
+                <span>with these awesome editing apps</span>
             </h2>
             <div class="row flex-wrap justify-center items-center">
                 <a href={routes.background} class="col w-full max-w-md space-y-3 group focus:outline-none m-5">
@@ -45,6 +47,7 @@ function AppSelector() {
                 </a>
             </div>
         </section>
+        <Shortcuts />
     </>
 }
 
@@ -61,7 +64,7 @@ function Header() {
         <div class="col-span-2 w-full text-center row justify-center">
             <a href={routes.home} class="outline-primary rounded">
                 <h1 class="row text-4xl font-cursive p-1 pr-2 space-x-3">
-                    <img src={srcLogo} alt="Panda Snap Logo" class="hidden sm:inline-block w-10 h-10 rounded-md overflow-hidden pointer-events-none" />
+                    <img src={srcLogo} alt="Pretty Snap Logo" class="w-10 h-10 rounded-md overflow-hidden pointer-events-none" />
                     <span>Pretty Snap</span>
                 </h1>
             </a>
@@ -84,4 +87,78 @@ function Footer() {
     return <footer class="pb-10 text-center space-y-3">
         <p>Found an issue or have a suggestion? <a class="text-gray-900 underline" href="https://github.com/kangabru/pretty-snap/issues/new">Let me know here!</a></p>
     </footer>
+}
+
+function Shortcuts() {
+    return <section class="col">
+        <h2 class="text-2xl">Quickly select an area to screenshot:</h2>
+        <div class="row justify-center flex-wrap text-gray-500">
+            <ShortcutGroup title="Windows" href="https://www.bloggingtriggers.com/quickly-take-screenshot-windows/">
+                <Win />
+                <span>+</span>
+                <Shift />
+                <span>+</span>
+                <Key text="S" />
+            </ShortcutGroup>
+
+            <ShortcutGroup title="Mac" href="https://support.apple.com/en-us/HT201361">
+                <MacCmd />
+                <span>+</span>
+                <Shift />
+                <span>+</span>
+                <Key text="3" />
+            </ShortcutGroup>
+
+            <ShortcutGroup title="Linux" href="https://www.wikihow.com/Take-a-Screenshot-in-Linux">
+                <Shift />
+                <span>+</span>
+                <KeyWrap class="p-3">Printscreen</KeyWrap>
+            </ShortcutGroup>
+        </div>
+    </section>
+}
+
+function ShortcutGroup({ title, href, children }: Children & { title: string, href: string }) {
+    return <a class="col mx-10 my-6 p-2 space-y-3 group rounded outline-primary" href={href}>
+        <span class="group-hover:underline text-blue-500">{title}</span>
+        <div class="flex items-center space-x-2">{children}</div>
+    </a>
+}
+
+function KeyWrap({ class: cls, children }: CssClass & Children) {
+    return <span class={join(cls, "h-12 rounded-lg border border-gray-500")}>{children}</span>
+}
+
+function Key({ text }: { text: string }) {
+    return <KeyWrap class="p-3"><span class="w-6 h-6 row justify-center leading-none">{text}</span></KeyWrap>
+}
+
+function Win() {
+    return <KeyWrap class="flex space-x-1 p-3">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" width="20" height="20" viewBox="0.2 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M17.8 20l-12 -1.5c-1 -.1 -1.8 -.9 -1.8 -1.9v-9.2c0 -1 .8 -1.8 1.8 -1.9l12 -1.5c1.2 -.1 2.2 .8 2.2 1.9v12.1c0 1.2 -1.1 2.1 -2.2 1.9z"></path>
+            <line x1="12" y1="5" x2="12" y2="19"></line>
+            <line x1="4" y1="12" x2="20" y2="12"></line>
+        </svg>
+        <span>Win</span>
+    </KeyWrap>
+}
+
+function Shift() {
+    return <KeyWrap class="flex space-x-1 p-3 pl-2">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M9 20v-8h-3.586a1 1 0 0 1 -.707 -1.707l6.586 -6.586a1 1 0 0 1 1.414 0l6.586 6.586a1 1 0 0 1 -.707 1.707h-3.586v8a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+        </svg>
+        <span>Shift</span>
+    </KeyWrap>
+}
+
+
+function MacCmd() {
+    return <KeyWrap class="flex items-center space-x-1 p-3">
+        <span class="h-6 -mt-2 text-3xl leading-none inline-block">⌘</span>
+        <span>Cmd</span>
+    </KeyWrap>
 }
