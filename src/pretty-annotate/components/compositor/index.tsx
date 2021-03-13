f51bb3f3ae92c513fb1ddc31099a4e7f3eeb3f9b
Scott Gibbs <scott@gibbz.biz>
2021-03-12 22:03:48 -0600

    Updated the Pretty Annotate export process to use the common export wrapper

--- src/pretty-annotate/components/compositor/index.tsx
+++ src/pretty-annotate/components/compositor/index.tsx
@@ -1,14 +1,11 @@
 import { h } from 'preact';
 import { useCallback } from 'react';
-import useMeasure from 'react-use-measure';
 import DropZone from '../../../common/components/drop-zone';
 import ExportWrapper from '../../../common/components/export';
 import { OUTER_BORDER_RADIUS } from '../../../common/constants';
-import useExport, { Exports } from '../../../common/hooks/use-export';
+import { Exports } from '../../../common/hooks/use-export';
 import { setWarningOnClose, useWarningOnClose } from '../../../common/hooks/use-misc';
-import useExportBorderRadius from '../../../common/hooks/use-round-corners';
 import { ChildrenWithProps, CssStyle, ForegroundImage } from '../../../common/misc/types';
-import { getRenderScale } from '../../../common/misc/utils';
 import { Shape } from '../../misc/types';
 import useAnnotateStore from '../../stores/annotation';
 import useOptionsStore from '../../stores/options';
@@ -22,34 +19,36 @@ export default function Compositor({ children }: ChildrenWithProps<Exports>) {
 
     const image = useOptionsStore(s => s.image)
     const setImage = useCallback((image: ForegroundImage) => useOptionsStore.setState({ image }), [])
+    const onExport = useCallback(() => setWarningOnClose(false), [])
 
     useWarningOnClose(!!image) // Assume they're editing if they've add an image
-    const [editorRef, { width }] = useMeasure()
-    const scale = getRenderScale(width, image?.width)
 
-    const renderBorderRadius = useExportBorderRadius()
-    const [exportRef, download, copy] = useExport({ scale, ...(image as any) }, () => setWarningOnClose(false))
+    return <ExportWrapper exportWidth={image?.width} exportHeight={image?.width} onExport={onExport}
 
-    return <div class="col w-full space-y-6">
-        <section ref={editorRef} style={{ borderRadius: OUTER_BORDER_RADIUS }}
-            class="max-w-screen-md block w-full mx-auto overflow-hidden shadow-md">
-            {image
-                ? <ViewerEditor />
-                : <DropZone class="bg-blue-200 p-5 sm:p-10 pb-0 sm:pb-0 rounded-t-lg" setImage={setImage}
-                    title={<img src={logo} class="max-w-sm mx-auto -mb-1" />}
-                    contentProps={{ "class": "bg-white rounded-t-lg" }} />}
-        </section>
+        renderClient={
+            ({ ref, download, copy }) => (
+                <div class="col w-full space-y-6">
+                    <section ref={ref} style={{ borderRadius: OUTER_BORDER_RADIUS }}
+                        class="max-w-screen-md block w-full mx-auto overflow-hidden shadow-md">
+                        {image
+                            ? <ViewerEditor />
+                            : <DropZone class="bg-blue-200 p-5 sm:p-10 pb-0 sm:pb-0 rounded-t-lg" setImage={setImage}
+                                title={<img src={logo} class="max-w-sm mx-auto -mb-1" />}
+                                contentProps={{ "class": "bg-white rounded-t-lg" }} />}
+                    </section>
 
-        {/** A hacky hidden element used by dom-to-image to render the image.
-         * We do this so we can set the image size exactly and render consistently on different browsers. */}
-        {image && <ExportWrapper ref={exportRef}
-            class="relative overflow-hidden" style={{ borderRadius: renderBorderRadius }}>
-            <Image style={{ width: image?.width / scale, height: image?.height / scale }} />
-            <Viewer />
-        </ExportWrapper>}
+                    {image && children({ download, copy })}
+                </div>
+            )}
 
-        {image && children({ download, copy })}
-    </div>
+        renderExport={
+            ({ ref, width, height }) => (
+                <div ref={ref as any}>
+                    <Image style={{ width, height }} />
+                    <Viewer />
+                </div>
+            )}
+    />
 }
 
 /** This components displays the image and allows for users to draw and edit annotations. */
